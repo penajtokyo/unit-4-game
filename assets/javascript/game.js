cbddff0f5c202c77586d3fc5bfe45e164e6c5744
PenajTokyo <penajtokyorm@gmail.com>
2018-08-19 20:24:10 -0600

    modified with comments to make it easier to understand, also with some CSS to make it nicers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+
+// my variables for my JQuery
 var targetNumber = "";
 var wins = 0;
 var losses = 0;
@@ -5,11 +7,12 @@ var counter = 0;
 var images = ["./assets/images/blue.png","./assets/images/green.png","./assets/images/red.png","./assets/images/yellow.png"]
 
 
-
+// This is the object that generates the target number
 function randomTargetNumber(){
     targetNumber = Math.floor(Math.random() *100) +20;
 };
 
+// this function set the random on the crystals and displays them
 function resetCrystals () {
     for (var i = 0; i < images.length; i++) {
         var crystal = $("<img>");
@@ -18,14 +21,16 @@ function resetCrystals () {
         crystal.attr("value", (Math.floor(Math.random() * 12) + 1));
         crystal.attr("height", "100");
         $(".crystal-images").append(crystal);
+        console.log("resetCrystalsworking")
     }
 }
-
+// setr the win and loss and the counter
 function resetHTML () {
-    $(".target-number").html(targetNumber);
+    $(".targetNumber").html(targetNumber);
     $(".win-lose-counter").html("<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>");
     $(".score-number").html(counter);
     $(".crystal-images").empty();
+    console.log("ResetHTMLworking")
 }
 
 function totalReset () {
@@ -33,12 +38,13 @@ function totalReset () {
     counter = 0;
     resetHTML ();
     resetCrystals ();
+    console.log("Totalresetisworking")
 }
 
 randomTargetNumber();
 	resetHTML ();
     resetCrystals ();
-    
+    // the if and else statement that allows for winning or losing
     function crystalClick () {
         counter += parseInt($(this).attr("value"));
 		$(".score-number").html(counter);
@@ -49,7 +55,8 @@ randomTargetNumber();
 		else if (counter > targetNumber) {
 			losses++;
 			totalReset();
-		};
+        };
+        console.log("clicks are working")
     };
     
     $(document).on("click", ".crystal", crystalClick);
